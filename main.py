796c4a53279e5907102839a6ec80c7255292efeb
revaprama <pramaulidiareva@gmail.com>
2024-09-21 15:31:27 +0700

    bismillah sukses

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # nilai = nilai + 5
 
 # if (> 90 and nilai <= 100nilai>80):
-#         print("selamat anda lulus,dengan nilai yang memuaskans")
+#         print("selamat anda lulus,dengan nilai yang memuaskan")
 # elif (nilai>80):
 #         print("selamat anda lulus")
 # else:
@@ -12,7 +12,7 @@
 #for loop
 # for i in range (10, 1,-2):
 #      print(i)
-#      print("HELLO WHILE")
+#      print("HELLO WORLD")
 
 # angka = 30
 # # WHILE (angka > 1):
@@ -21,10 +21,20 @@
 
 #{FUNCTION}
 # def
-def sayHello():
-    print ("hello reva")
-    print("selamat pagi")
+# def sayHello(param_nama,param_umur):
+#     print (f"hello {param_nama},umur: {param_umur}")
+#     print ("selamat pagi")
 # sayHello()
 
 # print("hello reva")
 # print("selamat pagi")
+
+# FUNGSI_2KALI2 = LAMBDA param_angka * 2
+# angka = 20 
+
+# print (FUNGSI_2KALI2 (angka))
+
+# fungsi_kurang4 = lambda param_angka -4
+
+# angka = 10 
+# print (fungsi_kurang4(angka))
