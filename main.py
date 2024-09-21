e9642370ca0c469053b2a1af29078be7554b629a
revaprama <pramaulidiareva@gmail.com>
2024-09-21 15:16:45 +0700

    bismillah sukses

---
+++ main.py
@@ -0,0 +1,30 @@
+# nilai = int(input("masukan nilai: "))
+
+# nilai = nilai + 5
+
+# if (> 90 and nilai <= 100nilai>80):
+#         print("selamat anda lulus,dengan nilai yang memuaskans")
+# elif (nilai>80):
+#         print("selamat anda lulus")
+# else:
+#         print("anda masih belum lulus tes,semangat dan coba lagi")
+
+#for loop
+# for i in range (10, 1,-2):
+#      print(i)
+#      print("HELLO WHILE")
+
+# angka = 30
+# # WHILE (angka > 1):
+# print(angka)
+# angka -= 1 #angka = angka -1 
+
+#{FUNCTION}
+# def
+def sayHello():
+    print ("hello reva")
+    print("selamat pagi")
+# sayHello()
+
+# print("hello reva")
+# print("selamat pagi")
